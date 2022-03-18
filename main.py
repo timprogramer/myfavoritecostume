a10fb5e7235e1dc4e314b0ff2b219b02a24bcb4e
Tim GF63 <tim@shatzir.com>
2022-03-18 18:44:42 +0900

    0318

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ mario=wrap.sprite.add("mario-1-big",300,250,"stand")
 
 @wrap.on_key_always(wrap.K_RIGHT)
 def right():
-    wrap.sprite.set_reverse_x(mario, False)
+    #if wrap.sprite.get_angle(mario)==180:
+    #wrap.sprite.set_angle(mario,0)
+   # wrap.sprite.set_reverse_y(mario,False)
+    if wrap.sprite.get_reverse_y(mario)==True:
+        wrap.sprite.set_reverse_y(mario,False)
+    wrap.sprite.set_reverse_x(mario,False)
     wrap.sprite.move(mario,10,0)
     wrap.sprite.set_costume(mario,"walk1")
 
 @wrap.on_key_always(wrap.K_LEFT)
 def left():
+    if wrap.sprite.get_reverse_y(mario)==True:
+        wrap.sprite.set_reverse_y(mario,False)
     wrap.sprite.set_reverse_x(mario,True)
     wrap.sprite.move(mario, -10, 0)
+    wrap.sprite.set_costume(mario, "walk1")
 
 @wrap.on_key_always(wrap.K_UP)
 def up():
